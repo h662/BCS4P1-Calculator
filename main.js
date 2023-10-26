065a6e4d70732daec272450fece4f60cce349290
h662 <h662hong@gmail.com>
2023-10-26 12:47:22 +0900

    clickNumber

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const resultEl = document.querySelector(".result");
 
 function inputResult(value) {
-  result = value || 0;
+  result = value || "0";
 
-  resultEl.innerText = result;
+  if (result.length > 16) {
+    resultEl.innerHTML = `${result.substring(0, 16)}...`;
+  } else {
+    resultEl.innerText = result;
+  }
+}
+
+function clickNumber(number) {
+  if (result === "0") result = "";
+
+  let numberValue = result + number;
+
+  inputResult(numberValue);
 }
 
 inputResult();
